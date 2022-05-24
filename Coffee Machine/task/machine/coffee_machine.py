f8b2c97393aff9a94d331171bd888cbd3178973d
RKIddle <asiatutors@gmail.com>
2022-05-24 17:01:46 +0700

    part3/6 done

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,10 +1,21 @@
-print("Write how much cups of coffee you will need.")
-num_cups = int(input())
-water = str(num_cups * 200)
-milk = str(num_cups * 50)
-beans = str(num_cups * 15)
-num_cups = str(num_cups)
-print("For " + num_cups + " of coffee you will need:")
-print(water + " ml of water")
-print(milk + " ml of milk")
-print(beans + " g of coffee beans")
+# Checking amounts of ingredients
+print("How much water is available?")
+water_now = int(input())
+# Checking amounts of ingredients
+water_now = int(input())
+milk_now = int(input())
+coffee_now = int(input())
+# Number of ordered cups of coffee
+num_cups_needed = int(input())
+# Calculation of number of cups that can be made
+# Water_used = ratio of water
+cups_made = min(water_now//200, milk_now//50, coffee_now//15)
+extra_cups = cups_made - num_cups_needed
+if cups_made == num_cups_needed:
+    print("Yes, I can make that amount of coffee")
+elif cups_made > num_cups_needed:
+    print(f"Yes, I can make that amount of coffee (and even {extra_cups} more than that)")
+elif cups_made < num_cups_needed:
+    print(f"No, I can make only {cups_made} cups of coffee")
+
+
